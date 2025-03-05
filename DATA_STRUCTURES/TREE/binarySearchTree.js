bf3746889fa5fe4a21c4d3852263f0f340786289
Abin Babu <abinbabuonline@gmail.com>
2025-03-05 18:45:23 +0530

    tree !

---
+++ DATA_STRUCTURES/TREE/binarySearchTree.js
@@ -0,0 +1,57 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+
+    isEmpty() {
+        return this.root === null
+    }
+
+    insert(value) {
+        const node = new Node(value);
+        if (this.isEmpty()) {
+            this.root = node
+        } else {
+            this.insertNode(this.root, node)
+        }
+    }
+
+    insertNode(root, node) {
+        if (node.value < root.value) {
+            if (root.left == null) {
+                root.left = node
+            } else {
+                this.insertNode(root.left, node)
+            }
+        } else {
+            if (root.right == null) {
+                root.right = node
+            } else {
+                this.insertNode(root.right, node)
+            }
+        }
+    }
+
+    levelOrder(){
+        let queue = []
+        queue.push(this.root);
+        while(queue.length){
+            const curr = queue.shift()
+            console.log(curr.value);
+            if(curr.left) {
+                queue.push(curr.left)
+            }
+            if(curr.right){
+                queue.push(curr.right)
+            }
+        }
+    }
+}
