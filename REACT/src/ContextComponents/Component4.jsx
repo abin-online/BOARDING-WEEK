171ba2708f16704653f6934c34bf99884f468a4c
Abin Babu <abinbabuonline@gmail.com>
2025-03-10 19:41:29 +0530

    context api

---
+++ REACT/src/ContextComponents/Component4.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import ComponentContext from './Context'
+
+const Component4 = () => {
+
+    const user = useContext(ComponentContext)
+  return (
+    <div>
+      <h1>Component4</h1>
+      <h2>Name: {user.name}</h2>
+    </div>
+  )
+}
+
+export default Component4
