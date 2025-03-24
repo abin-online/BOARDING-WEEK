ffb9869193d747a0c431780c4c408ccf9e69368a
Abin Babu <abinbabuonline@gmail.com>
2025-03-24 17:36:33 +0530

    middlewares

--- NODEJS/API/Header.js
+++ NODEJS/API/Header.js
@@ -10,19 +10,39 @@ const middleware = (req, res, next)=> {
             res.status(403).json({message: 'Forbidden'})
         }
     } catch (error) {
-        console.log(error)
+        next(error)
+    }
+}
+
+const blockPUT = (req, res, next) => {
+    try {
+        if(req.method == 'PUT'){
+            res.status(403).json({message: 'PUT is blocked'})
+        }else{
+            next()
+        }
+    } catch (error) {
+        next(error)
     }
 }
 
 app.use(middleware)
+app.use(blockPUT)
 
 app.post('/users', (req, res)=> {
     try {
         res.status(201).json({message : 'user created'})
     } catch (error) {
-        console.log(error)
+        next(error)
     }
 })
+
+
+app.use((err, req, res, next)=> {
+    console.log(err);
+    res.status(500).json({message:'Internal server error'})
+})
+
 
 app.listen(3333, ()=> {
     console.log('connected !')
